4335a1c8b0c0167fc0932a2c0bbd36ea061ca33a
Dawn Monroe <deetriplett@gmail.com>
2017-01-23 15:57:00 -0500

    add pet on click

--- app.js
+++ app.js
@@ -3,10 +3,29 @@ $('li').on('click', function() {
 	$(this).text("Clicked!").delay(800).fadeOut(400);
 });
 
-$('.loc').hover(
+$('.loc').hover(,/strong>
   function(){
     $(this).html("<strong>Location:</strong> Your house?!");
   },
   function() {
-    $(this).html("<strong>Location:</strong> Treehouse Adoption Center");
+    $(this).html("<strong>Location:</strong> the Adoption House");
 });
+
+//Grab info frmo form
+
+$('#add-pet').on('click', function() {
+	var $name = $('#pet-name').val();
+	var $species = $('#pet-species').val();
+	var $notes = $('#pet-notes').val();
+
+	// Assemble HTML of our new element with above variables
+	var $newPet = $(
+		'<section class ="six columns"><div class="card"><p><strong>Name:</strong>' + $name + ' </p> <p> <strong> Species: </strong> ' + $species + '</p>
+		<p> <strong> Notes: </strong> ' + $notes + '</p> <span class= "close">&times;</span></div></section>'
+	);
+
+	//Attach to markup 
+	$('#posted-pets').append($newPet);
+});	
+
+
