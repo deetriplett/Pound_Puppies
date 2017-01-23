d99902b3d587f1f1098f3e2d67d525d9c039e73e
Dawn Monroe <deetriplett@gmail.com>
2017-01-23 15:37:28 -0500

    add hover

--- app.js
+++ app.js
@@ -2,3 +2,11 @@
 $('li').on('click', function() {
 	$(this).text("Clicked!").delay(800).fadeOut(400);
 });
+
+$('.loc').hover(
+  function(){
+    $(this).html("<strong>Location:</strong> Your house?!");
+  },
+  function() {
+    $(this).html("<strong>Location:</strong> Treehouse Adoption Center");
+});
